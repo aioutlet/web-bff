861a768955da539ecc03e741127903512edf26e3
prasadhonrao <honrao.prasad@gmail.com>
2025-11-21 10:10:33 +0000

    refactor: update Dapr gRPC endpoint and application ID in run script

--- run.ps1
+++ run.ps1
@@ -9,13 +9,13 @@ $host.ui.RawUI.WindowTitle = "Web BFF"
 Write-Host "Starting Web BFF with Dapr..." -ForegroundColor Green
 Write-Host "Service will be available at: http://localhost:8080" -ForegroundColor Cyan
 Write-Host "Dapr HTTP endpoint: http://localhost:3580" -ForegroundColor Cyan
-Write-Host "Dapr gRPC endpoint: localhost:50080" -ForegroundColor Cyan
+Write-Host "Dapr gRPC endpoint: localhost:50013" -ForegroundColor Cyan
 Write-Host ""
 
 dapr run `
-  --app-id web-bff `
+  --app-id web-bff-service `
   --app-port 8080 `
-  --dapr-http-port 3513 `
+  --dapr-http-port 3580 `
   --dapr-grpc-port 50013 `
   --resources-path .dapr/components `
   --config .dapr/config.yaml `
